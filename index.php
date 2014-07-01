d41352dab9f03a204f9f8a95809a91b4fa489268
Tarin Poddar <tarinpoddar27@gmail.com>
2014-06-30 21:12:13 -0400

    Added index.php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello World</title>
+</head>
+<body>
+  <p>
+ Hey guys, I am setting up my GIT repository right now.
+This is homework for my 2nd lecture. I am learning Dynamic web applications. YAYYY!!!
+  </p>
+</body>
+</html>
